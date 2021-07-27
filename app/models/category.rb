1963e8566b0b2390fe9849c346a01d0ff350d0a7
Bénédicte Bouclon <bouclonbenedicte@gmail.com>
2021-07-27 18:47:02 +0200

    add coocon nested form

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,7 @@
 class Category < ApplicationRecord
-  has_many :questions
+  has_many :questions, inverse_of: :category
+  validates :title, presence: true
+  accepts_nested_attributes_for :questions, reject_if: :all_blank, allow_destroy: true
+
 end
+
