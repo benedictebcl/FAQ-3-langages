26bc2bea8d98e206dc115344a5fb318a68156133
Bénédicte Bouclon <bouclonbenedicte@gmail.com>
2021-07-28 23:54:27 +0200

    create translation keys

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+Question.destroy_all
+Category.destroy_all
+
+I18n.locale = :en
+car = Question.create(title: "What car is it ?", body: "It is a Fiat 500", category: Category.create(title: "Car"))
+
+I18n.locale = :fr
+car = Question.update(title: "Quelle voiture est-ce ?", body: "C'est une Fiat 500", category: Category.create(title: "Voiture"))
+
+I18n.locale = :it
+car = Question.update(title: "Che macchina è ?", body: "È una Fiat 500", category: Category.create(title: "Auto"))
